02c141250573d0e1c9dc56aaa7d66615fc78e483
Nikita Slivenko <lilimumulala@gmail.com>
2021-08-04 13:22:57 +0600

    made tabs

--- components/Main.js
+++ components/Main.js
@@ -1,25 +1,36 @@
-import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-export default function Main({navigation}) {
-    const goToProfile = ()=> {
-        navigation.navigate('Profile')
-    }
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>главная</Text>
-            <Button onPress={goToProfile} title="go to profile" />
-        </View>
-    )
+export default function Main({ navigation }) {
+  const goToProfile = () => {
+    navigation.navigate("Profile");
+  };
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>главная</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={goToProfile}
+      >
+          <Text>go to profile</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    text: {
-        color: "red"
-    },
-    container: {
-        flex: 1,
-        alignItems: "center",
-        padding: 40
-    }
-})
+  text: {
+    color: "red",
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 40,
+  },
+  button: {
+    borderRadius: 20,
+    backgroundColor: "red",
+    color: "#fff",
+    padding: 10,
+  },
+});
