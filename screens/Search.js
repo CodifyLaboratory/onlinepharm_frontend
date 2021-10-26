5a5c705c6b222189adb8059a55e6b3764379df70
Nikita Slivenko <lilimumulala@gmail.com>
2021-10-27 02:59:26 +0600

    make a search screen

--- screens/Search.js
+++ screens/Search.js
@@ -1,11 +1,21 @@
-import React from "react";
-import {View, StyleSheet, Text, SafeAreaView, Platform} from "react-native";
+import React, {useEffect, useState} from "react";
+import {View, StyleSheet, Text, SafeAreaView, Platform, TouchableOpacity} from "react-native";
 import Icon from 'react-native-vector-icons/EvilIcons';
 import {Input} from 'react-native-elements';
 import SearchImg from '../assets/search/search.svg'
 import {search} from "../styles/search";
+import Api from "../API";
 
 const Search = () => {
+
+  const [searchValue, setSearchValue] = useState('')
+  const [searchData, setSearchData] = useState([])
+  useEffect(() => {
+    Api.getData(`medications/?search=${searchValue}`)
+      .then(res => searchValue.length > 0 ? setSearchData(res.data) : setSearchData([]))
+    console.log(searchData, 'dataaa')
+  }, [searchValue])
+
   return (
     <SafeAreaView>
       <View style={search.container}>
@@ -20,11 +30,19 @@ const Search = () => {
           }
           inputStyle={{fontSize: 15,}}
           inputContainerStyle={search.input}
+          onChangeText={value => setSearchValue(value)}
         />
-        <View style={search.searchLogo}>
+        {searchData.length > 0 ? <View style={search.searchBlock}>
+          {searchData?.map(item => (
+            <TouchableOpacity style={search.searchItem} key={item.id}>
+              <Text style={search.itemText}>{item?.title}</Text>
+            </TouchableOpacity>
+          ))}
+        </View> : <View style={search.searchLogo}>
           <SearchImg style/>
           <Text style={search.searchLogoText}> Найдите желаемый продукт</Text>
-        </View>
+        </View>}
+
       </View>
     </SafeAreaView>
   );
